b95dab11016853cc1f8cbda49cda31c6be785cd7
aliazzat <aliazzat584@yahoo.com>
2018-11-24 03:00:13 +0300

    first commit

---
+++ project/src/myweb/servlet.java
@@ -0,0 +1,40 @@
+package myweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class servlet
+ */
+@WebServlet("/servlet")
+public class servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public servlet() {
+       
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		
+		PrintWriter printwriter = response.getWriter();
+		String MyName= request.getParameter("MyName");
+	     printwriter.println("My name is:" + MyName);
+		System.out.print("this is test");
+	}
+
+
+
+}
